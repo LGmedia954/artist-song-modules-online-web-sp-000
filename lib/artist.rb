f46e070999e360cbca7d7483dd3fa9e6a6d26e82
Leslie G <lesliegonzalez@me.com>
2020-08-30 03:27:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,11 +14,7 @@ class Artist
  def initialize
     self.class.all << self
     @songs = []
-end
-
-  # def self.find_by_name(name)
-    # @@artists.detect{|a| a.name == name}
-  # end
+  end
 
   def self.all
     @@artists
